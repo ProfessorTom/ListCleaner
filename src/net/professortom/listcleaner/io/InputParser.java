edcaeb387e0487c78f3f98000fe616f3c754aa9f
Tomas Gallucci <professortom@gmail.com>
2016-05-08 13:46:49 -0500

    First attempt at parser. Should work and be bug free, but this is code. Code is never bug free.

---
+++ src/net/professortom/listcleaner/io/InputParser.java
@@ -0,0 +1,36 @@
+package net.professortom.listcleaner.io;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import net.professortom.listcleaner.datastructures.BookRecord;
+
+public class InputParser {
+
+    public static List<BookRecord> parse(final String file) throws IOException {
+        final File f = new File(file);
+        final List<BookRecord> books = new ArrayList<>();
+
+        try (BufferedReader br = new BufferedReader(new FileReader(f))) {
+            //example line: Prelude to War by Robert T. Elson, 1
+            for(String line; (line = br.readLine()) != null; ) {
+                final String[] columns = line.split(",");
+                final String[] authorAndTitle = columns[0].split(" by ");
+
+                final BookRecord book = new BookRecord();
+                book.title = authorAndTitle[0];
+                book.author = authorAndTitle[1];
+                book.volume = Integer.parseInt(columns[1]);
+
+                books.add(book);
+            }
+        }
+
+        return books;
+
+    }
+}
