f6a0f85858ec7daacd7c000b6b7b0744a2682022
Tomas Gallucci <professortom@gmail.com>
2016-05-08 22:06:36 -0500

    First attempt at file writing

---
+++ src/net/professortom/listcleaner/io/OutputWriter.java
@@ -0,0 +1,33 @@
+package net.professortom.listcleaner.io;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.List;
+
+import net.professortom.listcleaner.datastructures.BookRecord;
+
+public class OutputWriter {
+
+    public static void writeCSVFile(final List<BookRecord> books, final String outputName) throws IOException {
+        final BookRecord columnHeaders = new BookRecord();
+        columnHeaders.author = "Author";
+        columnHeaders.title = "Title";
+        columnHeaders.volume = -1; //-1 sentinal value for header row
+
+        books.add(0, columnHeaders);
+
+        final File f = new File(outputName);
+
+        try(BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(f))) {
+            for(final BookRecord book : books) {
+                final StringBuilder sb = new StringBuilder();
+                sb.append(book.title).append(",");
+                sb.append(book.author).append(",");
+                sb.append(book.volume == -1 ? "Volume" : String.valueOf(book.volume)); //-1 sentinal value for header row
+                sb.append("\n");
+            }
+        }
+    }
+}
