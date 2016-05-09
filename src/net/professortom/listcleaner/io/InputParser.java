29755bc6d44d6e7bb6dca39cfffa410bb0157d38
Tomas Gallucci <professortom@gmail.com>
2016-05-08 23:17:44 -0500

    had array conversion wrong

--- src/net/professortom/listcleaner/io/InputParser.java
+++ src/net/professortom/listcleaner/io/InputParser.java
@@ -44,6 +44,6 @@ public class InputParser {
             }
         }
 
-        return (String[])nonBlankLines.toArray();
+        return nonBlankLines.toArray(new String[nonBlankLines.size()]);
     }
 }
